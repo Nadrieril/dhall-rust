7d0f38c1ff244f47bd3619515567246b498b493e
NanoTech <nanotech@nanotechcorp.net>
2016-12-06 04:09:01 -0600

    Print some error context

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,14 @@ use std::io::{self, Read};
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
-    let r = parser::parse_expr(&buffer);
-    println!("{:?}", r);
+    match parser::parse_expr(&buffer) {
+        Ok(e) => println!("{:?}", e),
+        Err(lalrpop_util::ParseError::User { error: lexer::LexicalError::Error(pos, e) }) => {
+            let context = &buffer[pos..::std::cmp::min(buffer.len(), pos + 20)];
+            println!("Unexpected token in {:?}: {:?}...", e, context);
+        }
+        Err(e) => println!("{:?}", e),
+    }
 
     /*
     expr <- case exprFromText (Directed "(stdin)" 0 0 0 0) inText of
