6e6094731a3b6a8ff2adc23f8ecae43b25d02731
Nadrieril <nadrieril+git@gmail.com>
2019-12-21 23:01:41 +0000

    Bump dhall version

--- dhall/src/lib.rs
+++ dhall/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/dhall/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/dhall/0.1.1")]
 #![feature(trace_macros)]
 #![feature(slice_patterns)]
 #![feature(never_type)]
