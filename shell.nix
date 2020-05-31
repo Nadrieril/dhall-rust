1d40832097eb9095bb0dea2e1c1524bb42a8f48c
Nadrieril <nadrieril+git@gmail.com>
2020-05-31 16:38:43 +0100

    nix: add dhall to environment

--- shell.nix
+++ shell.nix
@@ -1,10 +1,20 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+
+  dhall = pkgs.stdenv.mkDerivation rec {
+    name = "dhall-${version}";
+    version = sources.dhall.version;
+    src = sources.dhall;
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src/* $out/bin/
+    '';
+  };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     binutils cmake gcc gnumake openssl pkgconfig rustup
-    niv
+    niv dhall
   ];
 }
