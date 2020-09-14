f8d15978e94fa2bc2eba38d89b84c7f612ff88c9
Nadrieril <nadrieril+git@gmail.com>
2020-09-15 00:11:35 +0100

    Release dhall version 0.7.0

--- dhall/src/lib.rs
+++ dhall/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/dhall/0.6.0")]
+#![doc(html_root_url = "https://docs.rs/dhall/0.7.0")]
 #![allow(
     clippy::implicit_hasher,
     clippy::module_inception,
