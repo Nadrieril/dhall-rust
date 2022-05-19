360269385818275751f4346d4a5c886c0f83cbf4
Nadrieril <nadrieril+git@gmail.com>
2022-05-19 15:16:55 +0100

    release: dhall version 0.11.1

--- dhall/src/lib.rs
+++ dhall/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/dhall/0.11.0")]
+#![doc(html_root_url = "https://docs.rs/dhall/0.11.1")]
 #![allow(
     clippy::implicit_hasher,
     clippy::module_inception,
